6a6385abe7e6f9acdb1374efa8fdae763d091384
MateuszMiszczynski <miszczynski.job@gmail.com>
2025-12-14 18:46:04 +0100

    adding some scripts

--- linux-scripts/script.py
+++ linux-scripts/script.py
@@ -4,25 +4,19 @@ import os
 dir = "/home/vboxuser/test/log/"
 warning_counts = {}
 
-# Iteracja przez pliki w katalogu
 for filename in os.listdir(dir):
     fullpath = os.path.join(dir, filename)
 
-    # Sprawdzanie, czy jest to plik
     if os.path.isfile(fullpath):
         warning_count = 0
 
         try:
             with open(fullpath, "r", encoding="utf-8") as f:
-                # Iteracja przez linie w pliku
                 for line in f:
                     warning_count += line.count("WARNING")
         except (UnicodeDecodeError, OSError):
-            # Obsługuje błędy związane z odczytem pliku
             continue
 
-        # Zapisanie wyniku w słowniku
         warning_counts[filename] = warning_count
 
-# Wydrukowanie wyników
 print(warning_counts)
